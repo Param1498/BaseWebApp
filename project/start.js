eb423a792c22e916888224264bb8ff3a3f2c01c4
param <param1498@gmail.com>
2021-05-28 13:29:38 +0530

    messsage

--- project/start.js
+++ project/start.js
@@ -9,10 +9,14 @@ app.use(express.static(__dirname));
 app.set('views', __dirname + '/html');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/home', function(request, response) {
   response.render('pages/index');
 });
-
+app.get('/about', function(request, response) {
+  response.render('pages/about');
+});app.get('/project', function(request, response) {
+  response.render('pages/project');
+});
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
